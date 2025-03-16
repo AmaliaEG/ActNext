62d3fe6316a05dc8abd02bc9353691dd8615e5da
Jemborg123 <147314734+Jemborg123@users.noreply.github.com>
2025-03-17 00:24:32 +0100

    Settings placeholder and simple temporary navigation screen

--- ubifineFrontend/App.js
+++ ubifineFrontend/App.js
@@ -1,11 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import SettingsPage from './Settings'; // Your settings page
 
-export default function App() {
+const Stack = createStackNavigator();
+
+
+export default function App( {navigation}) {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.row}>
+        <View style={styles.buttonContainer}>
+          <Button
+            title="Show Settings"
+            onPress={() => navigation.navigate('Settings')} // Navigate to SettingsPage
+          />
+        </View>
+        <View style={styles.buttonContainer}>
+          <Button title="Button 2" onPress={() => alert('Button 2 pressed')} />
+        </View>
+      </View>
+      <View style={styles.row}>
+        <View style={styles.buttonContainer}>
+          <Button title="Button 3" onPress={() => alert('Button 3 pressed')} />
+        </View>
+        <View style={styles.buttonContainer}>
+          <Button title="Button 4" onPress={() => alert('Button 4 pressed')} />
+        </View>
+      </View>
     </View>
   );
 }
@@ -13,8 +37,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  row: {
+    flexDirection: 'row',
+  },
+  buttonContainer: {
+    margin: 10, // Adjust the margin as needed
   },
 });
